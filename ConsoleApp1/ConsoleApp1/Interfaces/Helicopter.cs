4d0fbeefb311c82548248cba1ade682254652ac9
JohnRivel <johnrivel52@gmail.com>
2026-06-27 15:27:08 +0800

    commit
    Signed-off-by: JohnRivel <johnrivel52@gmail.com>

--- ConsoleApp1/ConsoleApp1/Interfaces/Helicopter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TransportChallenge;
- 
-public class Helicopter : Vehicle, IFlyable, IDriveable
-{
-    public override string Move()
-    {
-        return "Flying in the sky.";
-    }
-}
